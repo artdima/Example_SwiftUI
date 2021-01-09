1621cc7911fec660cf74b7aca73be029edb2e186
Dmitriy Medyannik <>
2021-01-09 20:11:00 +0300

    Finish TestingCombine

--- TestingCombine/TestingCombine/ContentView.swift
+++ TestingCombine/TestingCombine/ContentView.swift
@@ -7,17 +7,67 @@
 
 import SwiftUI
 
-//ViewModel
+//Models
+struct User: Decodable, Identifiable {
+    let id: Int
+    let name: String
+}
+
+//ViewModels
 import Combine
 
 final class ViewModel: ObservableObject {
     @Published var time = ""
+    @Published var users: [User] = []
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    let formatter: DateFormatter = {
+        let df = DateFormatter()
+        df.timeStyle = .medium
+        return df
+    }()
     
     init() {
-        
+        setupPublishers()
+    }
+    
+    private func setupPublishers() {
+        setupTimerPublisher()
+        setupDataTaskPublisher()
+    }
+    
+    private func setupDataTaskPublisher() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data, response) in
+                guard let httpResponse = response as? HTTPURLResponse,
+                      httpResponse.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: [User].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { _ in } receiveValue: { users in
+                self.users = users
+            }
+            .store(in: &cancellables)
+
+    }
+    
+    private func setupTimerPublisher() {
+        Timer.publish(every: 1, on: .main, in: .default)
+            .autoconnect()
+            .receive(on: RunLoop.main)
+            .sink { value in
+                self.time = self.formatter.string(from: value)
+            }
+            .store(in: &cancellables)
     }
 }
 
+//Views
 struct ContentView: View {
     
     @StateObject var viewModel = ViewModel()
@@ -25,6 +75,9 @@ struct ContentView: View {
     var body: some View {
         Text(viewModel.time)
             .padding()
+        List(viewModel.users) { user in
+            Text(user.name)
+        }
     }
 }
 
