3465a17a5a4050c1fa37f90a7c9e8db4185c367e
Dmitry Medyannik <mail@artdima.ru>
2021-01-09 10:56:16 +0300

    Add project TestingCombine

---
+++ TestingCombine/TestingCombine/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TestingCombine
+//
+//  Created by Medyannik Dima on 09.01.2021.
+//  Copyright © 2021 Medyannik Dima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
