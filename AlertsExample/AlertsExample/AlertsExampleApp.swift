cc251e8211246b35a7e884e718755c5bd27e26cd
Dmitriy Medyannik <>
2021-01-10 13:06:39 +0300

    Add project AlertsExample

---
+++ AlertsExample/AlertsExample/AlertsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertsExampleApp.swift
+//  AlertsExample
+//
+//  Created by Medyannik Dmitri on 10.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct AlertsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
