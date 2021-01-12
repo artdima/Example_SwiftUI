93e796809d2014497b256695c58515bbe72733a4
Dmitriy Medyannik <>
2021-01-12 23:51:31 +0300

    Add project TelegramMacApp

---
+++ TelegramMacApp/TelegramMacApp/TelegramMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramMacAppApp.swift
+//  TelegramMacApp
+//
+//  Created by Medyannik Dmitri on 12.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct TelegramMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
