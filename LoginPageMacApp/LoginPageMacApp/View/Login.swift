079d8f6e4c1de4c98dfcbe111086752296b2d378
Dmitriy Medyannik <>
2021-01-14 19:54:59 +0300

    Configure main view

--- LoginPageMacApp/LoginPageMacApp/View/Login.swift
+++ LoginPageMacApp/LoginPageMacApp/View/Login.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct Login: View {
+    //Getting screen frame
+    var screen = NSScreen.main?.visibleFrame
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            VStack {
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
+            
+            VStack {
+                Spacer()
+                Image("welcome")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.leading, -35)
+                Spacer()
+            }
+            .frame(width: (screen!.width / 1.8) / 2)
+            .background(Color("lightblue"))
+        }
+        .frame(width: screen!.width / 1.8, height: screen!.height - 100)
     }
 }
 
