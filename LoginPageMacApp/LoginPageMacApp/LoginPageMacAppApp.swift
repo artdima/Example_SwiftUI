434f93887f76af37c1828642cbcdcd6ef3190177
Dmitriy Medyannik <>
2021-01-14 19:54:12 +0300

    Add project LoginPageMacApp

---
+++ LoginPageMacApp/LoginPageMacApp/LoginPageMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageMacAppApp.swift
+//  LoginPageMacApp
+//
+//  Created by Medyannik Dmitri on 14.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
