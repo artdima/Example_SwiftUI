72d6d6c0cb0ec0054cad4f205da55ae69b039a85
Dmitriy Medyannik <>
2021-01-15 17:00:54 +0300

    Add new project(Online Shop App)

---
+++ OnlineShopApp/OnlineShopApp/OnlineShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopAppApp.swift
+//  OnlineShopApp
+//
+//  Created by Medyannik Dmitri on 15.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
