16d6dfd97a0d9f43f501caac6abab2d101080b37
Dmitry Medyannik <>
2021-01-25 20:24:13 +0300

    Create new project Custom Tab Bar

---
+++ CustomTabBar/CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Medyannik Dmitri on 25.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
