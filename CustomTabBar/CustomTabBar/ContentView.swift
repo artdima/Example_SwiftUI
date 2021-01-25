fe74b739792964858b7468d4685f1f00efac5386
Dmitry Medyannik <mail@artdima.ru>
2021-01-25 21:02:42 +0300

    Add 4 buttons

--- CustomTabBar/CustomTabBar/ContentView.swift
+++ CustomTabBar/CustomTabBar/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            CustomTabs()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomTabs: View {
+    var body: some View {
+        HStack {
+            Button(action: {
+                
+            }, label: {
+                Image("icons8-asterix-50")
+            })
+            Button(action: {
+                
+            }, label: {
+                Image("icons8-futurama-bender-50")
+            })
+            Button(action: {
+                
+            }, label: {
+                Image("icons8-minion-50")
+            })
+            Button(action: {
+                
+            }, label: {
+                Image("icons8-woody-woodpecker-50")
+            })
+        }
     }
 }
