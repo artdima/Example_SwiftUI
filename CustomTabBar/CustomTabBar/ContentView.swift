bcb24169b1d193fd5554ffa0715ef325cb1103de
Dmitry Medyannik <mail@artdima.ru>
2021-01-26 22:02:30 +0300

    Add image

--- CustomTabBar/CustomTabBar/ContentView.swift
+++ CustomTabBar/CustomTabBar/ContentView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var index = 0
     var body: some View {
         VStack {
             Spacer()
-            CustomTabs()
+            CustomTabs(index: $index)
         }
     }
 }
@@ -23,28 +24,52 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct CustomTabs: View {
+    @Binding var index: Int
     var body: some View {
         HStack {
             Button(action: {
-                
+                self.index = 0
             }, label: {
-                Image("icons8-asterix-50")
+                Image(systemName: "books.vertical.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
             })
+            .foregroundColor(Color.black.opacity(self.index == 0 ? 1 : 0.2))
+            Spacer(minLength: 0)
+            
             Button(action: {
-                
+                self.index = 1
             }, label: {
-                Image("icons8-futurama-bender-50")
+                Image(systemName: "graduationcap")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
             })
+            .foregroundColor(Color.black.opacity(self.index == 1 ? 1 : 0.2))
+            Spacer(minLength: 0)
+            
             Button(action: {
-                
+                self.index = 2
             }, label: {
-                Image("icons8-minion-50")
+                Image(systemName: "cart")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
             })
+            .foregroundColor(Color.black.opacity(self.index == 2 ? 1 : 0.2))
+            Spacer(minLength: 0)
+            
             Button(action: {
-                
+                self.index = 3
             }, label: {
-                Image("icons8-woody-woodpecker-50")
+                Image(systemName: "gearshape")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
             })
+            .foregroundColor(Color.black.opacity(self.index == 3 ? 1 : 0.2))
         }
+        .padding(.horizontal, 35)
     }
 }
